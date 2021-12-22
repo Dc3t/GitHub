778d68d445f4ee36c1ea77b9f90ce14836d4cfda
BitcoinElon <95549461+BitcoinElon@users.noreply.github.com>
2021-12-22 13:38:16 +0300

    Update other.py

--- userbot/plugins/other.py
+++ userbot/plugins/other.py
@@ -9,43 +9,6 @@ from ..sql_helper.bot_pms_sql import (
     get_user_reply,
 )
 
-@iqthon.iq_cmd("dm",)
-async def dm(e):
-    if len(e.text) > 3 and e.text[3] != " ":  # weird fix
-        return
-    if len(e.text.split()) <= 1:
-        return await eor(e, get_string("dm_1"), time=5)
-    chat = e.text.split()[1]
-    try:
-        chat_id = await get_user_id(chat)
-    except Exception as ex:
-        return await eor(e, f"`{ex}`", time=5)
-    if e.reply_to:
-        msg = await e.get_reply_message()
-    elif len(e.text.split()) > 2:
-        msg = e.text.split(maxsplit=2)[2]
-    else:
-        return await eor(e, get_string("dm_2"), time=5)
-    try:
-        await e.client.send_message(chat_id, msg)
-        await eor(e, get_string("dm_3"), time=5)
-    except Exception as m:
-        await eor(e, get_string("dm_4").format(m, HNDLR), time=5)
-
-
-@iqthon.iq_cmd("fwdreply ?(.*)",)
-async def _(e):
-    message = e.pattern_match.group(1)
-    if not e.reply_to_msg_id:
-        return await eor(e, get_string("ex_1"), time=5)
-    if not message:
-        return await eor(e, get_string("dm_6"), time=5)
-    msg = await e.get_reply_message()
-    fwd = await msg.forward_to(msg.sender_id)
-    await fwd.reply(message)
-    await eor(e, get_string("dm_5"), time=5)
-
-
 @iqthon.iq_cmd(pattern="save$")
 async def saf(e):
     x = await e.get_reply_message()
