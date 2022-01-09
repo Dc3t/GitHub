1e8a56a0dcba30b38e9ca8763277370e2c4bc102
BitcoinElon <95549461+BitcoinElon@users.noreply.github.com>
2022-01-09 03:38:11 +0300

    Update test2.py

--- userbot/plugins/test2.py
+++ userbot/plugins/test2.py
@@ -2,6 +2,8 @@ import asyncio
 import os
 import random
 import shlex
+import math
+
 from typing import Optional, Tuple
 from PIL import Image, ImageDraw, ImageFont
 import PIL.ImageOps
@@ -651,3 +653,65 @@ async def memes(mafia):
         if files and os.path.exists(files):
             os.remove(files)
 
+@iqthon.on(admin_cmd(pattern="sin ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="sin ?(.*)", allow_sudo=True))
+async def findsin(event):
+    input_str = int(event.pattern_match.group(1))
+    output = math.sin(input_str)
+    await event.edit(f"**Value of Sin** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="cos ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="cos ?(.*)", allow_sudo=True))
+async def find_cos(event):
+    input_str = int(event.pattern_match.group(1))
+    output = math.cos(input_str)
+    await event.edit(f"**Value of Cos** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="tan ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="tan ?(.*)", allow_sudo=True))
+async def find_tan(event):
+    input_str = int(event.pattern_match.group(1))
+    output = math.tan(input_str)
+    await event.edit(f"**Value of Tan** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="cosec ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="cosec ?(.*)", allow_sudo=True))
+async def find_csc(event):
+    input_str = float(event.pattern_match.group(1))
+    output = mpmath.csc(input_str)
+    await event.edit(f"**Value of Cosec** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="sec ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="sec ?(.*)", allow_sudo=True))
+async def find_sec(event):
+    input_str = float(event.pattern_match.group(1))
+    output = mpmath.sec(input_str)
+    await event.edit(f"**Value of Sec** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="cot ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="cot ?(.*)", allow_sudo=True))
+async def find_cot(event):
+    input_str = float(event.pattern_match.group(1))
+    output = mpmath.cot(input_str)
+    await event.edit(f"**Value of Cot** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="square ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="square ?(.*)", allow_sudo=True))
+async def square(event):
+    input_str = float(event.pattern_match.group(1))
+    output = input_str * input_str
+    await event.edit(f"**Square of** `{input_str}`\n== `{output}`")
+
+
+@iqthon.on(admin_cmd(pattern="cube ?(.*)"))
+@iqthon.on(sudo_cmd(pattern="cube ?(.*)", allow_sudo=True))
+async def cube(event):
+    input_str = float(event.pattern_match.group(1))  # DANGEROUSJATT
+    output = input_str * input_str * input_str
+    await event.edit(f"**Cube of** `{input_str}`\n== `{output}`")
