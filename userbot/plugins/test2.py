977a00a3d591853b3a7ccf84282a4151b0924cbb
BitcoinElon <95549461+BitcoinElon@users.noreply.github.com>
2022-01-10 15:29:48 +0300

    Update test2.py

--- userbot/plugins/test2.py
+++ userbot/plugins/test2.py
@@ -726,7 +726,7 @@ async def memes(mafia):
         if files and os.path.exists(files):
             os.remove(files)
 
-@iqthon.on(admin_cmd(outgoing=True, pattern=r"^\.asupan$"))
+@iqthon.on(admin_cmd(outgoing=True, pattern=r"^\.فيديو$"))
 async def _(event):
     try:
         response = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
@@ -736,7 +736,7 @@ async def _(event):
         await event.edit("**لا يمكن العثور على إدخال الفيديو.**")
 
 
-@iqthon.on(admin_cmd(outgoing=True, pattern=r"^\.chika$"))
+@iqthon.on(admin_cmd(outgoing=True, pattern=r"^\.فيديو2$"))
 async def _(event):
     try:
         response = requests.get("https://api-tede.herokuapp.com/api/chika").json()
@@ -745,38 +745,9 @@ async def _(event):
     except Exception:
         await event.edit("**لا يمكن العثور على إدخال الفيديو.**")
 
-AUTOPFP_PACK = os.environ.get("AUTOPFP_PACK", None)
-if AUTOPFP_PACK is None:
-    PACK = [        "Epic-Space-Wallpaper",        "Acoustic-Guitar-Wallpaper-HD",        "Taylor-Guitar-Wallpaper",        "Classical-Music-Wallpapers-for-Desktop",        "Prs-Guitar-Wallpaper",        "Iron-Man-Wallpaper-1920x1080",        "Dodge-Challenger-Black-Hellcat-Wallpaper",        "V-for-Vendetta-Mask-Wallpaper",        "Toxic-Mask-Wallpapers",        "Minion-Desktop-Wallpaper",        "Epic-1080p-Wallpapers",       "Japanese-Desktop-Wallpaper",        "Cool-Gold-Cars-Wallpapers",        "Pretty-Wallpapers-for-iPhone-Quotes",        "dark-abstract-wallpaper",        "abstract-dark-wallpaper",        "abstract-wallpapers-and-screensavers",        "roaring-lion-wallpaper",        "wolves-screensavers-and-wallpaper",        "cool-wallpaper-for-men",        "Epic-1080p-Wallpapers",        "hacker-background",        "Vietnam-War-Wallpapers",        "War-of-the-Worlds-Wallpaper",        "War-Plane-Wallpaper",        "World-War-Ii-Wallpaper",        "Cool-War-Wallpapers",        "World-War-2-Wallpaper-HD",    ]
-else:
-    PACK = AUTOPFP_PACK
+@iqthon.on(admin_cmd(pattern="فايروس(?:\s|$)([\s\S]*)"))
+async def permalink(mention):
+    await edit_or_reply(mention, f"https://t.me/rxxrx/17")
 
 
-async def animepp():
-    os.system("rm -rf donot.jpg")
-    rnd = random.randint(0, len(PACK) - 1)
-    pack = PACK[rnd]
-    pc = requests.get("http://getwallpapers.com/collection/" + pack).text
-    f = re.compile("/\w+/full.+.jpg")
-    f = f.findall(pc)
-    fy = "http://getwallpapers.com" + random.choice(f)
-    print(fy)
-    if not os.path.exists("f.ttf"):
-        urllib.request.urlretrieve(            "https://github.com/rebel6969/mym/raw/master/Rebel-robot-Regular.ttf",            "f.ttf",        )
-    urllib.request.urlretrieve(fy, "donottouch.jpg")
 
-
-@iqthon.on(admin_cmd(pattern="ppr ?(.*)", outgoing=True))
-async def main(event):
-    await event.edit("**Starting Profile Pic Runner  ..**")  # Owner @Kraken_The_BadASS
-    while True:
-        try:
-            await animepp()
-            file = await event.client.upload_file("donottouch.jpg")
-            await event.client(
-                functions.photos.DeletePhotosRequest(                    await event.client.get_profile_photos("me", limit=1)                )            )
-            await event.client(functions.photos.UploadProfilePhotoRequest(file))
-            os.system("rm -rf donottouch.jpg")
-        except:
-            pass
-        await asyncio.sleep(120)  # Edit this to your required needs
